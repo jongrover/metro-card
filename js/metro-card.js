58e7fddce2b07def9b768083b1ca3b5c5d0f3d2b
jongrover <hello@jonathangrover.com>
2015-05-07 12:32:04 -0400

    update to give more info and format $

--- js/metro-card.js
+++ js/metro-card.js
@@ -4,17 +4,23 @@ $(function(){
       $remaining_amount = $('#remaining_amount'),
       $number_of_rides = $('#number_of_rides'),
       $result = $('#result'),
-      $rides = $('#rides'),
-      cost_of_single_ride = 2.75;
+      $more_rides = $('#more_rides'),
+      $rides_remaining = $('#rides_remaining'),
+      $balance_remaining = $('#balance_remaining'),
+      cost_per_single_ride = 2.75;
   
   $metro_card_calculator.submit(function (event) {
     event.preventDefault();
 
     var remaining_amount = parseFloat($remaining_amount.val()),
         number_of_rides = parseInt($number_of_rides.val()),
-        amount_to_add = (cost_of_single_ride * number_of_rides) - remaining_amount;
+        rides_left = remaining_amount / cost_per_single_ride,
+        left_over = remaining_amount % cost_per_single_ride,
+        amount_to_add = (cost_per_single_ride * number_of_rides) - remaining_amount;
 
-    $result.text(amount_to_add);
-    $rides.text(number_of_rides);
+    $rides_remaining.text(Math.round(rides_left));
+    $balance_remaining.text(accounting.formatMoney(left_over));
+    $result.text(accounting.formatMoney(amount_to_add));
+    $more_rides.text(number_of_rides);
   });
 });
